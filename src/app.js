3b81f0dc5cc4772747c4c7b8a510e894c8224f37
onelonelyowl <64985946+onelonelyowl@users.noreply.github.com>
2023-11-03 17:33:11 +0000

    completed 6.1 activate

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ const app = express();
 const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
-//TODO: Create your GET Request Route Below: 
+app.get('/restaurants', async (req, res) => {
+    res.json(await Restaurant.findAll())
+})
 
 
 
